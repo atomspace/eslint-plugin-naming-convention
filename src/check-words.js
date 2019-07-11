356071c2809337ce22cfbe9d8507e505133b468e
PyTechMike <pytechmike@gmail.com>
2019-07-11 15:54:31 +0300

    Correct handler of numbers, basic wordlists added

--- src/check-words.js
+++ src/check-words.js
@@ -1,19 +1,20 @@
 let concat = require('ramda').concat;
 
-let wordlist = require('../stores/wordlist1.json');
+let wordlist1 = require('../stores/wordlist1.json');
+let wordlist2 = require('../stores/most-used-words-in-programmong.json');
 
-let mostCommonWords = new Set(wordlist);
+let mostCommonWords = new Set(wordlist1, wordlist2);
 
 function wordToLowerCase (word) {
 	return word.toLowerCase();
 }
 
 function ultimateStringSplitter (string) {
-	let splittedString = string.replace(/([A-Z][a-z]|\d)/g, function (coincidence) {
+	let splittedString = string.replace(/([A-Z][a-z])/g, function (coincidence) {
 		return ` ${coincidence}`;
 	});
 
-	return splittedString.split(' ');
+	return splittedString = splittedString.replace(/\d+/g, '').split(' ');
 }
 
 function splitIdentifierOnWords (variable) {
