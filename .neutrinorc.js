27f0e32062fa53a0baab6d09990192dd0b03878f
constgen <const.gen@gmail.com>
2019-07-21 16:37:40 +0300

    Lint self

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -2,9 +2,12 @@ module.exports = {
 	use: [
 		['@atomspace/eslint', {
 			eslint: {
-				env: {
-               node: true
-            }
+				env: {node: true},
+				plugins: ['self'],
+				rules: {
+					'self/latin-var-name-only': 'warn',
+					'self/no-rerely-used-words': 'warn'
+				}
 			}
 		}], 
 		'@neutrinojs/node', 
