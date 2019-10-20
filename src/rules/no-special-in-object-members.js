a140009917955266d42ca3794e59c72e940b9101
Artefall <maximov.artyom007@gmail.com>
2019-10-20 21:57:56 +0300

    New rule add

---
+++ src/rules/no-special-in-object-members.js
@@ -0,0 +1,28 @@
+let specialSymbolsRegExp = /[!#%&()*+/=@^_]/;
+
+module.exports = {
+	meta: {
+		type: 'problem'
+	},
+	create (context) {
+		return {
+			VariableDeclaration (node) {
+				let objectProperties = node.declarations[0].init.properties;
+
+				objectProperties.forEach(property => {
+
+					let objectMemeber = property.key.value;
+					let objectMemberHaveSpecialSymbol = objectMemeber.match(specialSymbolsRegExp);
+
+					if (objectMemberHaveSpecialSymbol) {
+						context.report({
+							node,
+							message: 'object have special symbol'
+						});
+					}
+				});
+
+			}
+		};
+	}
+};
