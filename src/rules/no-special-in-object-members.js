8a52eb0fb35e140cf52858f6df45f06bc8e5ab8f
Artefall <maximov.artyom007@gmail.com>
2019-11-11 22:19:56 +0200

    Bug fix. Test add

--- src/rules/no-special-in-object-members.js
+++ src/rules/no-special-in-object-members.js
@@ -11,24 +11,26 @@ module.exports = {
 		return {
 			VariableDeclaration(node) {
 				let objectProperties = node.declarations[0].init.properties;
-
-				objectProperties.forEach(property => {
-
-					let objectMemeber = property.key.value;
-					let specialSymbolInObjectMember = objectMemeber.match(SPECIAL_SYMBOLS_REGEXP);
-
-
-					if (specialSymbolInObjectMember) {
-						let listOfSpecialSymbolsInObjectMember = specialSymbolInObjectMember.join('');
-						context.report({
-							node: node,
-							messageId: 'errorMessage',
-							data: {
-								listOfSpecialSymbolsInObjectMember: listOfSpecialSymbolsInObjectMember
-							}
-						});
-					}
-				});
+				
+				if (objectProperties){
+					objectProperties.forEach(property => {
+	
+						let objectMemeber = property.key.value;
+						let specialSymbolInObjectMember = objectMemeber.match(SPECIAL_SYMBOLS_REGEXP);
+	
+	
+						if (specialSymbolInObjectMember) {
+							let listOfSpecialSymbolsInObjectMember = specialSymbolInObjectMember.join('');
+							context.report({
+								node: node,
+								messageId: 'errorMessage',
+								data: {
+									listOfSpecialSymbolsInObjectMember: listOfSpecialSymbolsInObjectMember
+								}
+							});
+						}
+					});
+				}
 			}
 		};
 	}
--- test/no-special-in-object-member.test.js
+++ test/no-special-in-object-member.test.js
@@ -27,6 +27,11 @@ ruleTester.run('no-special-in-object-members', noSpecialInObjectMember, {
 		},
 		{
 			code: `let object = {'simpleMethod':() => 'method'}`
+		},{
+			code: ''
+		},
+		{
+			code:'let i = 0'
 		}
 	],
 	invalid: [
