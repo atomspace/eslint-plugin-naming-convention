0e29748ff14b32c0031bb30b43261a837735be88
Artefall <maximov.artyom007@gmail.com>
2019-11-18 18:20:36 +0200

    Bug fix

---
+++ src/rules/no-generic-verbs.js
@@ -0,0 +1,34 @@
+let utils = require('../utils');
+
+module.exports = {
+	meta: {
+		type: 'problem',
+		messages: {
+			errorMessage: `The identifier has generic verb: {{ genericVerb }}.`
+		}
+	},
+	create(context) {
+		return {
+			Identifier(node) {
+				const GENERIC_VERBS = ['get', 'set', 'array', 'check', 'validate', 'make', 'process', 'start'];
+				let variableName = node.name;
+
+				let splitedVariableName = utils.splitWord(variableName);
+
+				splitedVariableName.forEach(word => {
+					if (GENERIC_VERBS.includes(word)){
+						context.report({
+							node: node,
+							messageId: 'errorMessage',
+							data: {
+								genericVerb: word
+							}
+						});
+					}
+				});
+
+
+			}
+		};
+	}
+};
